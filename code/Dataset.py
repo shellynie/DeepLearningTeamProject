243d1421e3cb17e0f32382886cdb6beadb32626d
Jie-Xie <jiexie@andrew.cmu.edu>
2018-12-04 18:52:27 +0000

    runnable baseline_furniture.py

--- code/Dataset.py
+++ code/Dataset.py
@@ -1,11 +1,12 @@
 import os
 import numpy as np
+import csv
 import pandas as pd
 from PIL import Image
 import torch
 from torch.utils.data import Dataset
 
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Furniture128(Dataset):
 
@@ -28,9 +29,10 @@ class Furniture128(Dataset):
             pil_image = Image.open(image_path).convert('RGB')
             pil_image = self.transform(pil_image)
             return pil_image, label
-        except:
+        except Exception as ex:
+            # print(ex)
             return None, label
-            
+
 
 def collate_fn(seq_list):
     images_list, labels_list = zip(*seq_list)
@@ -40,7 +42,58 @@ def collate_fn(seq_list):
         if images_list[i] is not None:
             batch_images.append(images_list[i])
             batch_labels.append(labels_list[i])
-    return torch.stack(batch_images).to(DEVICE), torch.Tensor(batch_labels).long().to(DEVICE)
+    if len(batch_images) > 0:
+        return torch.stack(batch_images).to(DEVICE), torch.Tensor(batch_labels).long().to(DEVICE)
+    else:
+        return None, None
+
+# class Furniture128(Dataset):
+#     def __init__(self, preffix: str, transform=None):
+#         self.preffix = preffix
+        
+#         self.labels = {}
+#         if preffix != 'test':
+#             with open(f'../data/{preffix}.csv', newline='') as csvfile:
+#                 reader = csv.DictReader(csvfile)
+#                 for row in reader:
+#                     idx = int(row['image_id'])
+#                     id_label = int(row['label_id']) - 1
+#                     self.labels[idx] = id_label
+
+#         self.transform = transform
+
+#     def __len__(self):
+#         return len(self.labels)
+
+#     def __getitem__(self, idx):
+#         idx  = idx + 1
+#         p = f'../data/{self.preffix}/{idx}.jpg'
+#         try:
+#             img = Image.open(p)
+#             img = self.transform(img)
+#         except:
+#             return torch.zeros((3, 224, 224)), 200
+#         c, w, h = img.shape
+#         if c != 3 or w != 224 or h != 224:
+#             return torch.zeros((3, 224, 224)), 200
+
+#         target = self.label[s[idx] if len(self.labels) > 0 else -1
+
+#         return img, target
+
+# def collate_fn(batch):
+#     new_img = []
+#     new_target = []
+#     for img, target in batch:
+#         if target == 200:
+#             continue
+#         c, w, h = img.shape
+#         new_img.append(img.view(1, c, w, h))
+#         new_target.append(target)
+#     if len(new_img) > 0:
+#         return torch.cat(new_img).to(DEVICE), torch.from_numpy(np.array(new_target)).to(DEVICE)
+#     else:
+#         return None, None
 
 
 
--- code/baseline_furniture.py
+++ code/baseline_furniture.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F 
 import torch.optim as optim
 from torch.autograd import Variable
-from torch.optim.lr_scheduler import MultiStepLR
+from torch.optim.lr_scheduler import MultiStepLR, ReduceLROnPlateau
 from torch.nn.init import xavier_uniform_
 from torch.utils.data import Dataset, DataLoader
 import time
@@ -12,7 +12,6 @@ from tensorboardX import SummaryWriter
 
 import numpy as np 
 import os
-# from tensorboard_logging import *
 
 import torchvision
 import torchvision.transforms as transforms
@@ -25,32 +24,20 @@ ROOT_DIR = '/home/xiejie/project'
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 CKPT_DIR = os.path.join(ROOT_DIR, 'ckpts')
 NUM_EPOCHS = 80
-BATCH_SIZE = 64
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 128
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # DEVICE = 'cpu'
-VALIDATION_SPLIT = .2
 RANDOM_SEED = 42
 SHUFFLE_DATASET = True
 NUM_WORKERS = 0
-LR = 0.005
+LR = 0.002
 IMAGE_SIZE = 224
 LOG_FREQ = 5
+CKPT_FREQ = 4
+
 np.random.seed(42)
-
-gpu = DEVICE == 'cuda'
-if gpu:
-    print("Using GPU")
-else:
-    print("Using CPU")
-
-# normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-#                                  std=[0.229, 0.224, 0.225])
-# transform = transforms.Compose([
-#     transforms.Resize(256),
-#     transforms.CenterCrop(224),
-#     transforms.ToTensor(),
-#     normalize,
-# ])
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
 
 def preprocess_crop():
     # return transforms.Compose([
@@ -82,56 +69,12 @@ def preprocess_crop():
 
 transform = preprocess_crop()
 
-# trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-#                                         download=True, transform=transform)
-# cifar_test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
-#                                        download=True, transform=transform)
-
-# print(len(trainset))
-
-# class cifar_mask_dataset(Dataset):
-#     def __init__(self, indices):
-#         self.data = [trainset[i] for i in indices]
-
-#     def __getitem__(self, i):
-#         return self.data[i]
-
-#     def __len__(self):
-#         return len(self.data)
-
-# dataset_size = len(trainset)
-# indices = list(range(dataset_size))
-# split = int(np.floor(VALIDATION_SPLIT * dataset_size))
-# if SHUFFLE_DATASET :
-#     np.random.seed(RANDOM_SEED)
-#     np.random.shuffle(indices)
-# train_indices, val_indices = indices[split:], indices[:split]
-
-# cifar_train_dataset = cifar_mask_dataset(train_indices)
-# cifar_dev_dataset = cifar_mask_dataset(val_indices)
 furniture_train_dataset = Furniture128(DATA_DIR, 'train', transform)
 furniture_dev_dataset = Furniture128(DATA_DIR, 'valid', transform)
-
-
-# cifar_train_loader = torch.utils.data.DataLoader(cifar_train_dataset, batch_size=BATCH_SIZE,
-#                                           shuffle=True, num_workers=NUM_WORKERS)
-# cifar_dev_loader = torch.utils.data.DataLoader(cifar_dev_dataset, batch_size=BATCH_SIZE,
-#                                         shuffle=False, num_workers=NUM_WORKERS)
-
-# cifar_test_loader = torch.utils.data.DataLoader(cifar_test_dataset, batch_size=BATCH_SIZE,
-#                                          shuffle=False, num_workers=NUM_WORKERS)
 
 furniture_train_loader = DataLoader(furniture_train_dataset, shuffle=True, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, collate_fn=collate_fn)
 furniture_dev_loader = DataLoader(furniture_dev_dataset, shuffle=False, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, collate_fn=collate_fn)
 
-# classes = ('plane', 'car', 'bird', 'cat',
-#            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
-
-# print(len(cifar_train_loader), len(cifar_train_dataset))
-# print(len(cifar_dev_loader), len(cifar_dev_dataset))
-# print(len(cifar_test_loader), len(cifar_test_loader))
 print('train size: {}'.format(len(furniture_train_dataset)))
 print('dev size: {}'.format(len(furniture_dev_dataset)))
 
@@ -171,10 +114,9 @@ class Densenet121(nn.Module):
         return self.classifier(out.view(out.size(0), -1))
 
 
-
 class Trainer():
     def __init__(self, model, optimizer, criterion, name, 
-        trainLoader, devLoader, load_path=None, scheduler=None):
+        trainLoader, devLoader, scheduler, load_path=None):
         self.model = model
         if load_path is not None:
             self.model.load_state_dict(torch.load(load_path))
@@ -183,15 +125,11 @@ class Trainer():
         self.name = name
         self.trainLoader = trainLoader
         self.devLoader = devLoader
+        self.scheduler = scheduler
         self.logger = SummaryWriter()
         self.best_model_path = None
 
-    # def save_model(self):
-    #     torch.save(self.model.state_dict(), get_model_path(self.name))
-
     def run(self, n_epochs):
-        print("Start Training...")
-        # tLog = Logger("./logs/train_{0}".format(self.name))
         best_acc = 0
         best_epoch = 0
         for i in range(n_epochs):
@@ -207,57 +145,47 @@ class Trainer():
                     continue
                 num_nonempty_batches += 1
                 self.optimizer.zero_grad()
-                X = Variable(batch_data)
-                Y = Variable(batch_label)
-                num_samples += X.size(0)
+                num_samples += batch_data.size(0)
 
                 self.model.train()
-                trainOutput = self.model(X)
+                trainOutput = self.model(batch_data)
                 pred = trainOutput.data.max(1, keepdim=True)[1]
-                predicted = pred.eq(Y.data.view_as(pred))
+                predicted = pred.eq(batch_label.view_as(pred))
                 batch_correct = predicted.sum().item()
                 batch_size = predicted.size(0)
                 correct += batch_correct
 
-                trainLoss = self.criterion(trainOutput, Y)
+                trainLoss = self.criterion(trainOutput, batch_label)
                 trainLossVal = trainLoss.data.item()
                 trainLoss.backward()
                 self.optimizer.step()
                 epoch_loss += trainLossVal
                 if i_batch % LOG_FREQ == 0:
-                    print('batch: {0}, train loss: {1:.4f}, train acc: {2:.4f}'.format(i_batch, trainLossVal, batch_correct / batch_size))
+                    print('Batch: {0}, Train Loss: {1:.4f}, Train Acc: {2:.4f}'.format(i_batch, epoch_loss / num_nonempty_batches, correct / num_samples))
             train_loss = epoch_loss / num_nonempty_batches
-            train_acc = correct.data.item() / num_samples
+            train_acc = correct / num_samples
             val_loss, val_acc = inference(self.model, self.criterion, self.devLoader)
-            print("epoch: {0}, train loss: {1:.8f}, train acc: {2:.8f}, val loss: {3:.8f}, val acc: {4:.8f}".format(i+1, train_loss, train_acc, val_loss, val_acc))
+            print("Epoch: {0}, Train Loss: {1:.8f}, Train Acc: {2:.8f}, Val Loss: {3:.8f}, Val Acc: {4:.8f}".format(i+1, train_loss, train_acc, val_loss, val_acc))
             hours, rem = divmod(time.time() - start_time, 3600)
             minutes, seconds = divmod(rem, 60)
             print('epoch train time: {:0>2}:{:0>2}:{:05.2f}'.format(int(hours), int(minutes), seconds))
-            self.logger.add_scalar("train_loss", train_loss, i+1)
-            self.logger.add_scalar("train_acc", train_acc, i+1)
-            self.logger.add_scalar("val_loss", val_loss, i+1)
-            self.logger.add_scalar("val_acc", val_acc, i+1)
+            self.logger.add_scalar("train/loss", train_loss, i+1)
+            self.logger.add_scalar("train/acc", train_acc, i+1)
+            self.logger.add_scalar("val/loss", val_loss, i+1)
+            self.logger.add_scalar("val/acc", val_acc, i+1)
+            self.scheduler.step(val_acc)
             for tag, value in self.model.named_parameters():
                 tag.replace('.', '/')
                 self.logger.add_histogram(tag, value.data.cpu().numpy(), i+1)
                 # self.logger.add_histogram(tag+'/grad', value.grad.data.cpu().numpy(), i+1)
-            if val_acc > best_acc:
+            if val_acc > best_acc and i % CKPT_FREQ == 0:
                 best_acc = val_acc
                 best_epoch = i + 1
-                self.best_model_path = os.path.join(CKPT_DIR, "{0}_model_epoch{1}_valacc{2}.pt".format(self.name, best_epoch, best_acc))
+                self.best_model_path = os.path.join(CKPT_DIR, "{0}_model_epoch{1}_valacc{2:.4f}.pt".format(self.name, best_epoch, best_acc))
                 torch.save(self.model.state_dict(), self.best_model_path)
                 print("Saved ckpts to {}".format(self.best_model_path))
             end_time = time.time()
             print("time used: {0}s".format(end_time - start_time))
-
-
-# def get_model_path(name):
-#     return os.path.join(CKPT_DIR, "{0}_model_epoch.pt".format(name))
-
-
-# def get_prediction_path(name):
-#     return "./{0}_pred.npy".format(name)
-
 
 def inference(model, criterion, devLoader):
     correct = 0
@@ -265,20 +193,19 @@ def inference(model, criterion, devLoader):
     dev_size = 0
     num_nonempty_batches = 0
     num_samples = 0
-    for i_batch, (batch_data, batch_label) in enumerate(devLoader):
-        if batch_data is None:
-            continue
-        num_nonempty_batches += 1
-        X = Variable(batch_data)
-        Y = Variable(batch_label)
-        num_samples += X.size(0)
+    with torch.no_grad():
         model.eval()
-        out = model(X)
-        pred = out.data.max(1, keepdim=True)[1]
-        predicted = pred.eq(Y.data.view_as(pred))
-        correct += predicted.sum()
-        loss += criterion(out, Y).data.item()
-    return (loss / num_nonempty_batches, correct.data.item() / num_samples)
+        for i_batch, (batch_data, batch_label) in enumerate(devLoader):
+            if batch_data is None:
+                continue
+            num_nonempty_batches += 1
+            num_samples += batch_data.size(0)
+            out = model(batch_data)
+            pred = out.data.max(1, keepdim=True)[1]
+            predicted = pred.eq(batch_label.view_as(pred))
+            correct += predicted.sum().item()
+            loss += criterion(out, batch_label).data.item()
+        return (loss / num_nonempty_batches, correct / num_samples)
 
 
 # def predict(mlp, name, loader, load_path):
@@ -302,32 +229,31 @@ def inference(model, criterion, devLoader):
 
 
 def train_model1(train_loader, dev_loader):
-    name = "baseline_resnet101_1"
-    # load_path = "baseline11_model_epoch17_err0.04889741.pt"
-    # trainer = Trainer(resnet_3.cuda(), None, None, name, None, load_path)
-    # model = trainer.model.cuda()
+    name = "baseline_resnet101"
     model = Resnet101(init=True).to(DEVICE)
+    if torch.cuda.device_count() > 1:
+        print('Using {} GPUs.'.format(torch.cuda.device_count()))
+        model = nn.DataParallel(model)
     optimizer = optim.Adam(model.parameters(),lr=LR)
     # scheduler = ReduceLROnPlateau(optimizer, 'min', patience=3)
     scheduler = MultiStepLR(optimizer, milestones=[38, 53], gamma=0.1)
     criterion = nn.CrossEntropyLoss()
     trainer = Trainer(model, optimizer, criterion, name, train_loader, dev_loader, scheduler=scheduler)
     trainer.run(NUM_EPOCHS)
-    # trainer.save_model()
 
 def train_model2(train_loader, dev_loader):
-    name = "baseline_densenet121_1"
+    name = "baseline_densenet121"
     model = Densenet121(init=True).to(DEVICE)
+    if torch.cuda.device_count() > 1:
+        print('Using {} GPUs.'.format(torch.cuda.device_count()))
+        model = nn.DataParallel(model)
     optimizer = optim.Adam(model.parameters(),lr=LR)
-    # optimizer = optim.SGD(model.parameters(), lr=0.001, nesterov=True, momentum=0.9)
     # scheduler = ReduceLROnPlateau(optimizer, 'min', patience=3)
     scheduler = MultiStepLR(optimizer, milestones=[38, 53], gamma=0.1)
     criterion = nn.CrossEntropyLoss()
     trainer = Trainer(model, optimizer, criterion, name, train_loader, dev_loader, scheduler=scheduler)
     trainer.run(NUM_EPOCHS)
-    # trainer.save_model()
 
 if __name__ == "__main__":
     train_model2(furniture_train_loader, furniture_dev_loader)
-    # predict(Resnet101().cuda(), "baseline_resnet101_1", cifar_test_loader)
-    print("check")
+    print('All finished!')
